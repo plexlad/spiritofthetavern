4ed68175684b3c8ede522df3e2bf2744829ade8a
plexlad <brock@tylerjsmith.me>
2023-02-04 14:04:31 -0700

    quiet time feature is now functional!

--- src/scripts/quiettime.js
+++ src/scripts/quiettime.js
@@ -39,8 +39,8 @@ function quietTime(client, on) {
 
 module.exports = {
 	ready: function (client) {
-		client.vars.startTimes = []; // times where quiet time should be enabled
-		client.vars.stopTimes = []; // checks when quiet time should be disables
+		let startTimes = []; // times where quiet time should be enabled
+		let stopTimes = []; // checks when quiet time should be disables
 		
 		// runs through all of the times in config
 		// checks if that time has already passed on the current day. 
@@ -49,17 +49,17 @@ module.exports = {
 			let today = new Date()
 			let parsedTime = Date.parse(`${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()} ${times[0]}:${today.getSeconds()}-07:00`);
 			if ((parsedTime - today.getTime()) <= 0) {
-				client.vars.startTimes.push(parsedTime + (8.64e+7));
+				startTimes.push(parsedTime + (8.64e+7));
 			} else {
-				client.vars.startTimes.push(parsedTime);
+				startTimes.push(parsedTime);
 			}
 			
 			// does the same as stated above, but with the stop times
 			parsedTime = Date.parse(`${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()} ${times[1]}:${today.getSeconds()}-07:00`);
 			if ((parsedTime - today.getTime()) <= 0) {
-				client.vars.stopTimes.push(parsedTime + (8.64e+7));
+				stopTimes.push(parsedTime + (8.64e+7));
 			} else {
-				client.vars.stopTimes.push(parsedTime);
+				stopTimes.push(parsedTime);
 			}
 
 		}
@@ -67,21 +67,20 @@ module.exports = {
 		// does a check for the start and disable times every 30 seconds
 		// with the way that the system is designed, the time delay will not matter
 		setInterval(() => {
-			for (const time of client.vars.startTimes) {
+			for (const time of startTimes) {
 				if (Date.now() >= time) {
 					quietTime(client, true);
-					client.vars.startTimes[client.vars.startTimes.indexOf(time)] = time + 8.64e+7;
+					startTimes[startTimes.indexOf(time)] = time + 8.64e+7;
 				}
 			}
 			
-			// TODO: looks like quiet time is stuck in a loop. it's probably here, so fix it'
-			for (const time of client.vars.stopTimes) {
+			for (const time of stopTimes) {
 				if (Date.now() >= time) {
 					quietTime(client, false);
-					client.vars.startTimes[client.vars.startTimes.indexOf(time)] = time + 8.64e+7;
+					stopTimes[stopTimes.indexOf(time)] = time + 8.64e+7;
 				}
 			}
-		}, 30000);
+		}, 15000);
 	},
 	quietTime
 }
